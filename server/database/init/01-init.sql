beb00de01023445b54ad2f77827cbc620fe19ec9
lionel sisso <sissolionel@gmail.com>
2026-01-13 05:12:48 +0100

    fixed wrong docker files

--- server/database/init/01-init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS AcademVault CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS 'academ_vault_user'@'%' IDENTIFIED BY 'Secret123!';
-GRANT ALL PRIVILEGES ON AcademVault.* TO 'academ_vault_user'@'%';
-FLUSH PRIVILEGES;
